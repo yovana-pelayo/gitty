21f17731c3c78596236afdd6f86518e5afa9af2b
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-07-11 01:15:05 -0700

    created a test that redirects to github upon login and uses tokens to instead of sharing passwords

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,14 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('github tests routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should login and redirect to github oauth page upon login ', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(/https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
   afterAll(() => {
     pool.end();
